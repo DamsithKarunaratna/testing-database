cd81d2a4d7f6c275342652b9acdec045257792d6
dammakaru <dammakaru@gmail.com>
2017-04-29 11:08:08 +0530

    Add database + rs2xml via upload

---
+++ ims.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2017 at 05:36 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `description` int(4) NOT NULL,
+  `debit` int(11) DEFAULT NULL,
+  `credit` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_ID` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `catName` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_ID`, `catName`) VALUES
+(00003, 'toffees'),
+(00004, 'creams'),
+(00005, 'DVD'),
+(00006, 'VCD'),
+(00007, 'Audio CD'),
+(00008, 'VHS'),
+(00009, 'Pianos'),
+(00012, 'Phones');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventlog`
+--
+
+CREATE TABLE `eventlog` (
+  `Username` varchar(30) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `itemCode` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `itemName` varchar(30) NOT NULL,
+  `price` int(10) NOT NULL,
+  `stock` int(10) NOT NULL,
+  `reorderLevel` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`itemCode`, `category`, `itemName`, `price`, `stock`, `reorderLevel`) VALUES
+(0001, 'Phones', 'iPhone 6s', 50000, 2000, 200),
+(0006, 'toffees', 'Bulto', 5, 2000, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `oID` int(5) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sID` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `itemCode` int(4) UNSIGNED ZEROFILL NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `returns`
+--
+
+CREATE TABLE `returns` (
+  `rID` int(5) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sID` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `itemCode` int(4) UNSIGNED ZEROFILL NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplier_ID` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `addressLine1` varchar(30) NOT NULL,
+  `addresssLine2` varchar(30) NOT NULL,
+  `addressLine3` varchar(30) NOT NULL,
+  `contactNo` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `debt` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Username` varchar(30) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `AccessLevel` int(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Username`, `Password`, `AccessLevel`) VALUES
+('Saman', 'Saman123', 1),
+('Nimal', 'Nimal123', 2),
+('Nayana', 'Nayana123', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_ID`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`itemCode`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oID`);
+
+--
+-- Indexes for table `returns`
+--
+ALTER TABLE `returns`
+  ADD PRIMARY KEY (`rID`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `usernames` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_ID` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `itemCode` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplier_ID` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
